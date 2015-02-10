94681b34a799f920de0275647523ce0ac4aa0ab0
itamaev2 <itamaev2@gmail.com>
2015-02-10 12:38:06 +0000

    neobundleの設定を最新化した

--- setup.sh
+++ setup.sh
@@ -5,4 +5,16 @@ if [ ! -d ~/.dotfiles ];  then
   exit 1
 fi
 
+which git
+if [ $? -ne 0 ]; then
+  echo "git入ってない"
+  exit 1
+fi
+
+# vim
 ln -s ~/.dotfiles/.vimrc ~/.
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/swp
+mkdir -p ~/.vim/backup
+
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
